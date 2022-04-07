49c79965d85e1b7c5abe17b687aa9bdb3f6bce15
Zsolt Szende <zsolt@artificial.io>
2022-04-07 17:58:22 +0200

    Add neovim

--- home.nix
+++ home.nix
@@ -105,6 +105,27 @@ with builtins; {
 
     jq.enable = true;
 
+    neovim = {
+      enable = true;
+      extraConfig = ''
+        colorscheme gruvbox
+        let g:context_nvim_no_redraw = 1
+        set mouse=a
+        set number
+        set termguicolors
+      '';
+      plugins = with pkgs.vimPlugins; [
+        editorconfig-vim
+        gruvbox-community
+        vim-airline
+        haskell-vim
+        vim-nix
+      ];
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+    };
+
     vscode = {
       enable = true;
       # To update extensions.json run: bin/vscode_extensions.sh

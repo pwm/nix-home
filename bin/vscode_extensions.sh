9e7359b2b8b9221acba5e839790ea167c320b1fe
Zsolt Szende <zsolt@artificial.io>
2023-11-08 12:34:40 +0000

    updates

--- bin/vscode_extensions.sh
+++ bin/vscode_extensions.sh
@@ -49,7 +49,9 @@ echo "found at $vs_code"
 echo "Downloading and updating extensions:"
 json='['
 for i in $($vs_code --list-extensions); do
-  if [ "$i" == "undefined_publisher.brossa-language" ]; then continue; fi
+  if [[ "$i" == Artificial* ]]; then continue; fi
+  if [[ "$i" == *brossa-language ]]; then continue; fi
+  if [[ "$i" == *brossa-lsp ]]; then continue; fi
   echo " - $i ..."
   owner=$(echo "$i" | cut -d. -f1)
   ext=$(echo "$i" | cut -d. -f2)

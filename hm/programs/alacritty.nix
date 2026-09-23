1d7a0baa33e03667bd19ac087f0bccae88aa5d0d
Zsolt Szende <zsolt@artificial.io>
2026-09-23 23:13:14 +0100

    Keep alacritty's [terminal] settings from the toml when setting the shell
    The settings merged the toml with a plain //, which replaced the whole
    [terminal] table (and with it terminal.osc52 = "CopyPaste") with just
    shell.program. Use lib.recursiveUpdate so both survive.

--- hm/programs/alacritty.nix
+++ hm/programs/alacritty.nix
@@ -2,7 +2,9 @@
 {
   enable = true;
 
-  settings = (pkgs.lib.importTOML ./alacritty/alacritty.toml) // {
+  # recursiveUpdate, as a plain // would replace the whole [terminal] table
+  # from the toml (and with it terminal.osc52) with just shell.program.
+  settings = pkgs.lib.recursiveUpdate (pkgs.lib.importTOML ./alacritty/alacritty.toml) {
     terminal.shell.program = "/Users/${user}/.nix-profile/bin/fish";
   };
 }
